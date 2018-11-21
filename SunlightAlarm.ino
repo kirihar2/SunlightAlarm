2bf303b043f42f0e31df592544f14ee450e0dc8c
kirihar2 <kirihara.juran@gmail.com>
2018-11-21 15:08:37 -0600

    update

--- SunlightAlarm.ino
+++ SunlightAlarm.ino
@@ -52,12 +52,12 @@ BlynkTimer timer;
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
-char auth[] = "f926197c1ae94bc28b2a8a71d83689fb";
+char auth[] = "";
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
-char ssid[] = "NETGEAR62";
-char pass[] = "gracefulroad125";
+char ssid[] = "";
+char pass[] = "";
 
 #define ledPin D1
 
@@ -96,7 +96,7 @@ struct Settings {
   -21600,
 };
 
-#include "C:\Users\juran\Documents\Arduino\NodeMCU\Sunlight\NodeMCU2\libdcc\animation.h"
+#include "libdcc/animation.h"
 
 WiFiUDP Udp;
 unsigned int localPort = 8888;  // local port to listen for UDP packets
